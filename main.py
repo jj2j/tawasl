9b5215fb63348b435c54c17b75a6d60a1b54ad24
jj2j <88926466+jj2j@users.noreply.github.com>
2022-01-25 17:43:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import  random, time, user_agent,requests
 from user_agent import generate_user_agent
 ID = '1818416834'
 
-token = '1771143161:AAHmMU5t0kZF50Rjl8BtOdSQgWOaHmLaZgY'
+token = ''5238529821:AAHCfWYnOi1xjDKYWlYpWRj2C37HGc7I1ko'
 bot = telebot.TeleBot(token)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 ###start###
 start_msg = '''
 𝐰𝐞𝐥𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐢𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 𝐡𝐮𝐜𝐤 𝐚𝐜𝐜𝐨𝐮𝐧𝐭𝐬 𝐛𝐨𝐭  ᵈᵉᵛ @BQBXB
@@ -23,3 +26,14 @@ def hello(message):
 	voice = 'https://t.me/bqbzb/2'
 	bot.send_voice(message.chat.id, voice, '@BQBXB')
 @bot.message_handler(commands=['hunt'])
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://tawasl.herokuapp.com/"+str(BOT_TOKEN))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
