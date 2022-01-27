5bff9c1fa706a8e6029db929f74ff569161c8052
jj2j <88926466+jj2j@users.noreply.github.com>
2022-01-27 10:58:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def redirect_message():
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url="https://tawasl.herokuapp.com/"+str(BOT_TOKEN))
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
